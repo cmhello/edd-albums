20710eb3b55021dd7ca31b9235a96186d7299468
Spencer Finnell <spencerfinnell@gmail.com>
2015-01-24 10:45:37 -0700

    Marketify support

--- edd-albums.php
+++ edd-albums.php
@@ -36,18 +36,23 @@ class EDD_Albums {
 
 		$this->suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
+		// textdomain
+		add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
+
+		// admin
 		add_filter( 'edd_download_price_table_head', array( $this, 'edd_download_price_table_head' ) );
 		add_action( 'edd_download_price_table_row', array( $this, 'edd_download_price_table_row' ), 10, 3 );
 		add_filter( 'edd_price_row_args', array( $this, 'edd_price_row_args' ), 10, 2 );
 		
+		// frontend
 		add_action( 'edd_after_price_option', array( $this, 'edd_after_price_option' ), 10, 3 );
-
-		add_action( 'admin_enqueue_scripts', array( $this, 'edd_load_admin_scripts' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
-
 		add_filter( 'edd_purchase_download_form', array( $this, 'edd_purchase_download_form' ), 10, 2 );
 
-		add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
+		add_filter( 'body_class', array( $this, 'body_class' ) );
+
+		// scripts
+		add_action( 'admin_enqueue_scripts', array( $this, 'edd_load_admin_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
 	}
 	
 	public function load_plugin_textdomain() {
@@ -55,12 +60,6 @@ class EDD_Albums {
 
 		load_textdomain( 'edd-albums', WP_LANG_DIR . "/edd-albums/edd-albums-$locale.mo" );
 		load_plugin_textdomain( 'edd-albums', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-	}
-
-	public function edd_purchase_download_form( $form, $args ) {
-		$form = str_replace( 'class="edd_download_purchase_form ', 'class="edd_download_purchase_form playlist ', $form );
-
-		return $form;
 	}
 
 	public function wp_enqueue_scripts() {
@@ -101,6 +100,20 @@ class EDD_Albums {
 
 	public function edd_download_price_table_head( $post_id ) {
 		echo '<th width="55">' . __( 'Preview', 'edd-albums' ) . '</th>';
+	}
+
+	public function body_class( $classes ) {
+		$theme = wp_get_theme();
+
+		$classes[] = 'theme-' . sanitize_title( $theme->Name );
+
+		return $classes;
+	}
+
+	public function edd_purchase_download_form( $form, $args ) {
+		$form = str_replace( 'class="edd_download_purchase_form ', 'class="edd_download_purchase_form playlist ', $form );
+
+		return $form;
 	}
 
 	public function edd_after_price_option( $key, $price, $download_id ) {
